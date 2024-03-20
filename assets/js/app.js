c72ff062b4068820f289829eccf184298b1ae871
shodibek <baxtishodcoder@gmail.com>
2024-03-20 13:44:22 +0500

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,111 @@
+let week = prompt("Hafta kunini kiriting!").trim().toLowerCase();
+switch (week) {
+    case "dushanba":
+        console.log("Monday");
+        break;
+    case "seshanba":
+        console.log("Thursday");
+        break
+    case "chorshanba":
+        console.log("Wednesday");
+        break
+    case "payshanba":
+        console.log("Thursday");
+        break
+    case "juma":
+        console.log("Week");
+        break
+    case "shanba":
+        console.log("Saturday");
+        break
+    case "yakshanba":
+        console.log("Sunday");
+        break
+    default:
+        console.log("Bunaqa hafta kuni yuq");
+}
+
+
+
+
+
+// lowercase
+// uppercase
+// pascalcase
+// camelcase
+// const btnel = "salom";
+// const BTNEL = "salom";
+// let ButtonElement = "btnasfsfdhg";
+// let btnEl = "srdtfygfhjgj";
+
+// const inputEl = document.querySelector(".input");
+
+// inputEl.addEventListener("input", () => {
+//   console.log(inputEl.value);
+// });
+
+// const selectEl = document.querySelector(".select");
+
+// selectEl.addEventListener("change", () => {
+//   console.log(selectEl.value);
+// });
+
+// let num = 20;
+
+// console.log(`mening yoshim ${num} ${num} da`);
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// arr.push("salom");
+
+// console.log(arr);
+
+// let numArr = [1232, 5432, 23, 44, 123, 543, 213, 543];
+
+// console.log(numArr.sort((a, b) => a - b));
+
+// let data = new Date();
+
+// let yil = data.getFullYear();
+// let oy = data.getMonth() + 1;
+// let kun = data.getDay();
+// let soat = data.getHours();
+// let minut = data.getMinutes();
+// let sekund = data.getSeconds();
+// let milsekund = data.getMilliseconds();
+
+// let res =
+//   yil +
+//   ":" +
+//   oy +
+//   ":" +
+//   kun +
+//   ":" +
+//   soat +
+//   ":" +
+//   minut +
+//   ":" +
+//   sekund +
+//   ":" +
+//   milsekund;
+
+// console.log(res);
+// let arr = [
+//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+// ];
+// let juft = [];
+// let toq = [];
+
+// for (let i = 0; i <= arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     juft.push(arr[i]);
+//   } else {
+//     toq.push(arr[i]);
+//   }
+// }
+
+// console.log(juft);
+// console.log(toq);
+
+// switch case
+
